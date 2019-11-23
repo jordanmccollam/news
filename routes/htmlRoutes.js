a3c1f9a8b8e05feb76993b5b883a40e7603ff29a
Jordan McCollam <jordy.mccollam@gmail.com>
2019-11-22 23:22:41 -0600

    Setting up index to take database info

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,10 +1,17 @@
 var axios = require("axios");
 var cheerio = require("cheerio");
+var db = require("../models");
 
 module.exports = function(app) {
 
     app.get("/", function(req, res) {
-        res.render("index");
+        db.Article.find({}, function(err, found) {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render("index", found);
+            }
+        })
     })
 
 }
